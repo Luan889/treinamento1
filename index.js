143cc714857f0d176f82600b2101db0e2fa8d923
Luan Moreira dos Santos <luan.santos@grupovab.local>
2022-04-08 16:03:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var texto1 = document.getElementById("b1");
+var email = document.getElementById("b2");
+
+//metodo para mostrar um alerta treinamento
+function teste(){
+    alert("Formulario enviado com sucesso!" + texto1.value + "" + email.value);
+}
